2ff1bf4499de485e2480580379a53f3599036b88
SwathiSukumaran <SwathiSukumaran@EINCQA1L-SAWS>
2025-05-06 08:18:36 +0530

    first commit

---
+++ JavaMavenProject/src/main/java/samplePackage/SampleClass.java
@@ -0,0 +1,18 @@
+package samplePackage;
+
+public class SampleClass {
+	public static void add()
+	{
+		int a=10;
+		int b=15;
+		int c=a+b;
+		System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+		SampleClass.add();
+		
+
+	}
+
+}
